c35d44c7b4168c788c9b7e230582c1ce7ed58333
Harvey Mei <harvey.mei@msn.com>
2021-05-27 16:59:21 +0800

    Commit Message

--- forex_rate.py
+++ forex_rate.py
@@ -101,12 +101,14 @@ def rate_output():
     # ax1.fill_between(date_list, usd_b_list, usd_s_list, facecolor='blue', alpha=0.1)  # 填充颜色
     ax1.set_ylabel("USD/CNY", fontsize=16)
     ax1.grid(True)  # 绘制网格
+    ax1.legend(('buy_rate', 'sell_rate'))
 
     ax2 = plt.subplot(312, sharex=ax1)  # 绘图（3行，1列，第2幅）,共享第一幅x轴
     ax2.plot(date_list, eur_b_list, c='red')
     ax2.plot(date_list, eur_s_list, c='blue')
     ax2.set_ylabel("EUR/CNY", fontsize=16)
     ax2.grid(True)
+    ax2.legend(('buy_rate', 'sell_rate'))
 
     ax3 = plt.subplot(313, sharex=ax1)  # 绘图（3行，1列，第3幅）,共享第一幅x轴
     ax3.plot(date_list, hkd_b_list, c='red')
@@ -114,6 +116,7 @@ def rate_output():
     ax3.set_ylabel("HKD/CNY", fontsize=16)  # 添加子图y轴标签
     ax3.set_xlabel("Weekday", fontsize=16)  # 仅在第三幅图显示x轴标签
     ax3.grid(True)
+    ax3.legend(('buy_rate', 'sell_rate'))
     fig.autofmt_xdate()
 
     # plt.show()
