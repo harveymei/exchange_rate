c8c51051155b0ca74cf47380b1558c67f5d299a1
Harvey Mei <harvey.mei@msn.com>
2021-05-27 09:49:17 +0800

    Commit Message

--- file_reader.py
+++ file_reader.py
@@ -36,21 +36,23 @@ fig = plt.figure(figsize=(10, 6), dpi=128)
 fig.suptitle("Ping An Bank Forex Rate Per Day")
 ax1 = plt.subplot(311)  # 绘图（3行，1列，第1幅）
 ax1.plot(date_list, usd_b_list, c='red')
-ax1.plot(date_list, usd_s_list, c='green')
+ax1.plot(date_list, usd_s_list, c='blue')
+# ax1.fill_between(date_list, usd_b_list, usd_s_list, facecolor='blue', alpha=0.1)  # 填充颜色
 ax1.set_ylabel("USD/CNY")
-ax1.fill_between(date_list, usd_b_list, usd_s_list, facecolor='blue', alpha=0.1)  # 填充颜色
 ax1.grid(True)  # 绘制网格
 
-ax2 = plt.subplot(312, sharex=ax1)
+ax2 = plt.subplot(312, sharex=ax1)  # 绘图（3行，1列，第2幅）,共享第一幅x轴
 ax2.plot(date_list, eur_b_list, c='red')
-ax2.plot(date_list, eur_s_list, c='green')
+ax2.plot(date_list, eur_s_list, c='blue')
 ax2.set_ylabel("EUR/CNY")
+ax2.grid(True)
 
 
-ax3 = plt.subplot(313, sharex=ax1)
+ax3 = plt.subplot(313, sharex=ax1)  # 绘图（3行，1列，第3幅）,共享第一幅x轴
 ax3.plot(date_list, hkd_b_list, c='red')
-ax3.plot(date_list, hkd_s_list, c='green')
+ax3.plot(date_list, hkd_s_list, c='blue')
 ax3.set_ylabel("HKD/CNY")  # 添加子图y轴标签
+ax3.grid(True)
 
 # plt.show()
 plt.savefig(datetime.datetime.now().strftime("%Y%m%d%H%M%S") + ".png")
