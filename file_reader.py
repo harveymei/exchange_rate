27c4d351ea7df91f6d6c87651ce5a5955cfd1e9b
Harvey Mei <harvey.mei@msn.com>
2021-05-27 10:25:54 +0800

    Commit Message

--- file_reader.py
+++ file_reader.py
@@ -26,33 +26,36 @@ with open(filename) as f_object:
         hkd_b_list.append(data['hkd_cny']['buy_rate'])
         hkd_s_list.append(data['hkd_cny']['sell_rate'])
 
-    print(date_list, usd_b_list)
+    # print(date_list, usd_b_list)
 
 # 两个或多个共享一个轴的图（解决不同y值差距较大，单图因比例问题影响细节显示）
+# Subplots Layout 多图布局
 # https://matplotlib.org/stable/gallery/subplots_axes_and_figures/share_axis_lims_views.html
 # https://matplotlib.org/stable/gallery/subplots_axes_and_figures/shared_axis_demo.html
 # 三位整数（行，列，索引），可逗号分隔或整体传入参数
 fig = plt.figure(figsize=(10, 6), dpi=128)
-fig.suptitle("Ping An Bank Forex Rate Per Day")
+fig.suptitle("Ping An Bank Forex Rate Per Day", fontsize=24)
+
 ax1 = plt.subplot(311)  # 绘图（3行，1列，第1幅）
 ax1.plot(date_list, usd_b_list, c='red')
 ax1.plot(date_list, usd_s_list, c='blue')
 # ax1.fill_between(date_list, usd_b_list, usd_s_list, facecolor='blue', alpha=0.1)  # 填充颜色
-ax1.set_ylabel("USD/CNY")
+ax1.set_ylabel("USD/CNY", fontsize=16)
 ax1.grid(True)  # 绘制网格
 
 ax2 = plt.subplot(312, sharex=ax1)  # 绘图（3行，1列，第2幅）,共享第一幅x轴
 ax2.plot(date_list, eur_b_list, c='red')
 ax2.plot(date_list, eur_s_list, c='blue')
-ax2.set_ylabel("EUR/CNY")
+ax2.set_ylabel("EUR/CNY", fontsize=16)
 ax2.grid(True)
 
 
 ax3 = plt.subplot(313, sharex=ax1)  # 绘图（3行，1列，第3幅）,共享第一幅x轴
 ax3.plot(date_list, hkd_b_list, c='red')
 ax3.plot(date_list, hkd_s_list, c='blue')
-ax3.set_ylabel("HKD/CNY")  # 添加子图y轴标签
+ax3.set_ylabel("HKD/CNY", fontsize=16)  # 添加子图y轴标签
 ax3.grid(True)
+fig.autofmt_xdate()
 
 # plt.show()
 plt.savefig(datetime.datetime.now().strftime("%Y%m%d%H%M%S") + ".png")
