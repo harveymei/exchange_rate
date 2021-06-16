9889bdc2f944386144ec9da943da20b74ba490fc
Harvey Mei <harvey.mei@msn.com>
2021-06-16 12:15:47 +0800

    Commit Message

--- forex_rate.py
+++ forex_rate.py
@@ -50,12 +50,18 @@ def rate_input():
         sell_rate_4 = float(input("请输入HKD卖出价："))
         input_value_4 = {"buy_rate": buy_rate_4, "sell_rate": sell_rate_4}
 
+        input_key_5 = "gbp_cny"
+        buy_rate_5 = float(input("请输入GBP买入价："))
+        sell_rate_5 = float(input("请输入GBP卖出价："))
+        input_value_5 = {"buy_rate": buy_rate_5, "sell_rate": sell_rate_5}
+
         # 拼接字典键值赋值变量
         rate_record = {
             input_key_1: input_value_1,
             input_key_2: input_value_2,
             input_key_3: input_value_3,
-            input_key_4: input_value_4
+            input_key_4: input_value_4,
+            input_key_5: input_value_5
         }
 
         # 将字典作为列表元素追加写入列表
@@ -72,7 +78,10 @@ def rate_output():
     """
     filename = 'data.json'
     date_list = []
-    usd_b_list, usd_s_list, eur_b_list, eur_s_list, hkd_b_list, hkd_s_list = [], [], [], [], [], []
+    usd_b_list, usd_s_list = [], []
+    eur_b_list, eur_s_list = [], []
+    hkd_b_list, hkd_s_list = [], []
+    gbp_b_list, gbp_s_list = [], []
 
     with open(filename) as f_object:
         full_data = json.load(f_object)
@@ -86,6 +95,16 @@ def rate_output():
             eur_s_list.append(data['eur_cny']['sell_rate'])
             hkd_b_list.append(data['hkd_cny']['buy_rate'])
             hkd_s_list.append(data['hkd_cny']['sell_rate'])
+            # 添加异常处理，处理数据文件早期无英镑汇率数据的问题
+            try:
+                gbp_b_list.append(data['gbp_cny']['buy_rate'])
+            except KeyError:  # 读取数据产生KeyError异常（不存在）时忽略异常并在列表追加写入0值
+                gbp_b_list.append(0)
+
+            try:
+                gbp_s_list.append(data['gbp_cny']['sell_rate'])
+            except KeyError:
+                gbp_s_list.append(0)
 
         # print(date_list, usd_b_list)
 
@@ -94,10 +113,11 @@ def rate_output():
     # https://matplotlib.org/stable/gallery/subplots_axes_and_figures/share_axis_lims_views.html
     # https://matplotlib.org/stable/gallery/subplots_axes_and_figures/shared_axis_demo.html
     # 三位整数（行，列，索引），可逗号分隔或整体传入参数
-    fig = plt.figure(figsize=(10, 6), dpi=128)  # 设置图表尺寸及解析度（点数每英寸，1英寸=2.54厘米）
+    fig = plt.figure(figsize=(10, 18), dpi=128)  # 设置图表尺寸及解析度（点数每英寸，1英寸=2.54厘米）
+    # 新增英镑汇率后，调整为10:(6*3)比例
     fig.suptitle("Ping An Bank Forex Rate Per Weekday", fontsize=24)  # 设置多图标题
 
-    ax1 = plt.subplot(311)  # 绘图（3行，1列，第1幅）
+    ax1 = plt.subplot(411)  # 绘图（4行，1列，第1幅）
     ax1.plot(date_list, usd_b_list, c='red')
     ax1.plot(date_list, usd_s_list, c='blue')
     # ax1.fill_between(date_list, usd_b_list, usd_s_list, facecolor='blue', alpha=0.1)  # 填充颜色
@@ -107,7 +127,7 @@ def rate_output():
     # 图例位置参数loc默认为最佳位置（自动）
     ax1.legend(('buy_rate', 'sell_rate'), fontsize=8, loc='lower left')  # 添加图例，指定字体大小及位置（左下方）
 
-    ax2 = plt.subplot(312, sharex=ax1)  # 绘图（3行，1列，第2幅）,共享第一幅x轴
+    ax2 = plt.subplot(412, sharex=ax1)  # 绘图（4行，1列，第2幅）,共享第一幅x轴
     # 颜色设置
     # https://matplotlib.org/stable/tutorials/colors/colors.html
     # https://matplotlib.org/stable/gallery/color/named_colors.html （颜色名称列表）
@@ -117,14 +137,26 @@ def rate_output():
     ax2.grid(True)
     ax2.legend(('buy_rate', 'sell_rate'), fontsize=8, loc='lower left')
 
-    ax3 = plt.subplot(313, sharex=ax1)  # 绘图（3行，1列，第3幅）,共享第一幅x轴
+    ax3 = plt.subplot(413, sharex=ax1)  # 绘图（4行，1列，第3幅）,共享第一幅x轴
     ax3.plot(date_list, hkd_b_list, c='m')  # magenta 洋红色
     ax3.plot(date_list, hkd_s_list, c='k')  # black 黑色
     ax3.set_ylabel("HKD/CNY", fontsize=16)  # 添加子图y轴标签
-    ax3.set_xlabel("Weekday", fontsize=16)  # 仅在第三幅图显示x轴标签
+    # ax3.set_xlabel("Weekday", fontsize=16)  # 仅在第三幅图显示x轴标签
     ax3.grid(True)
     ax3.legend(('buy_rate', 'sell_rate'), fontsize=8, loc='lower left')
-    fig.autofmt_xdate()  # 仅在第3幅图处绘制斜的x轴日期标签，默认参数值为右对齐旋转30度
+    # fig.autofmt_xdate()  # 仅在第3幅图处绘制斜的x轴日期标签，默认参数值为右对齐旋转30度
+
+    # 新增英镑汇率
+    ax4 = plt.subplot(414, sharex=ax1)
+    ax4.plot(date_list, gbp_b_list, c='m')
+    ax4.plot(date_list, gbp_s_list, c='k')
+
+    ax4.set_ylabel("GBP/CNY", fontsize=16)
+    ax4.set_xlabel("Weekday", fontsize=16)  # 调整为仅在第四幅图显示x轴坐标
+    ax4.grid(True)
+    ax4.legend(("buy_rate", 'sell_rate'), fontsize=8, loc='lower left')
+    fig.autofmt_xdate()  # 调整为仅在第4幅图处绘制斜的x轴日期标签，默认参数值为右对齐旋转30度
+
 
     # plt.show()
     # 使用精确到秒的当前时间作为随机文件名
